19358a64c3d401645168b3920979a40074741a97
Fernando <Fernando>
2017-01-16 21:22:47 -0200

    exercise 3 development in R script

--- mean.R
+++ mean.R
@@ -1,2 +1,46 @@
+######WUR Geo-scripting course
 ##Paulo Bernardino
 ##January 16th 2017
+
+#Exercise3
+
+is.leap<-function(year){
+  #Function to determine whether or not any year is a leap year
+  #Determining that years before 1582 are "out of the valid range" years
+  if(year<=1581){
+    print(paste(year,"is out of the valid range"))
+  }else{
+  #Checking if the argument is numeric
+    if(is.numeric(year)==FALSE){
+    print(paste("Error: argument of class numeric expected"))
+  }else{
+  #Calculating if the year is a leap year
+    if(year %% 4 == 0) {
+    if(year %% 100 == 0) {
+      if(year %% 400 == 0) {
+  #Printing the result
+        print(paste("TRUE"))
+      } else {
+        print(paste("FALSE"))
+      }
+    } else {
+      print(paste("TRUE"))
+    }
+  } else {
+    print(paste("FALSE"))
+  }
+}
+
+f1<-function(year){
+  if(is.numeric(year)==FALSE){
+    print(paste("False"))
+ }else{
+  print(paste("True"))
+}
+}
+}
+}
+
+is.leap(2000)
+is.leap(1581)
+is.leap('john')
